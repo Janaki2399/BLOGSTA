d55094ac8da1ce22835a9396446bfa376ef6535a
Janaki <janakishreeja23@gmail.com>
2019-02-14 22:16:25 +0530

    commit

---
+++ app/src/main/java/com/example/blogsta/Cse.java
@@ -0,0 +1,13 @@
+package com.example.blogsta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cse);
+    }
+}
