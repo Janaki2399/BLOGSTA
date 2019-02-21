a464cf9de22669f4d79610e0e51e7a1c8c1405e7
Janaki <janakishreeja23@gmail.com>
2019-02-21 17:22:51 +0530

    commit

--- app/src/main/java/com/example/blogsta/Blog_guide.java
+++ app/src/main/java/com/example/blogsta/Blog_guide.java
@@ -1,13 +1,26 @@
 package com.example.blogsta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Blog_guide extends AppCompatActivity {
-
+Button blog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_guide);
+
+        blog = (Button) findViewById(R.id.blog);
+
+        blog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Blog_guide.this, SectionActivity.class));
+            }
+        });
+
     }
 }
